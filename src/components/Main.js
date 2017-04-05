eca04845329db3667a8abc643da97e948493d92e
serege <prototype9449@yandex.ru>
2017-04-05 20:11:26 +0300

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+require('normalize.css/normalize.css')
+import '../styles/main'
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+AppComponent.defaultProps = {
+}
+
+export default AppComponent
---
+++ src/redux/transition.js
@@ -0,0 +1,13 @@
+export const CHANGE_STATE_LINE = 'CHANGE_STATE_LINE'
+export const CHANGE_TRANSITIONSLINE = 'CHANGE_TRANSITIONSLINE'
+export const ADD_TRANSITION = 'ADD_TRANSITION'
+
+const initialState = {
+  loaded: false
+};
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+
+  }
+}
